05e4b7c0aa1ba0da4f806c19b0d4b9caed90d3ad
= <=>
2024-05-15 11:34:54 +0100

    second commit

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./asset/css/style.scc">
+    <link rel="stylesheet" href="./assets/CSS/style.css">
     <title>Blog</title>
 </head>
 <body>
-    
+ 
 </body>
 </html>
